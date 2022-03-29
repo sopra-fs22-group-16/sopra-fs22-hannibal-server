0baeaa67166b4ea2a009fa3cb3883cf8a56eba56
Alessio Brazerol <alessio.bra@hotmail.ch>
2022-03-29 20:52:08 +0200

    Deleted unnecessary code from template

--- src/main/java/ch/uzh/ifi/hase/soprafs22/constant/UserStatus.java
+++
@@ -1,5 +0,0 @@
-package ch.uzh.ifi.hase.soprafs22.constant;
-
-public enum UserStatus {
-  ONLINE, OFFLINE, ONE_VS_ONE;
-}
--- src/main/java/ch/uzh/ifi/hase/soprafs22/rest/mapper/DTOMapper.java
+++ src/main/java/ch/uzh/ifi/hase/soprafs22/rest/mapper/DTOMapper.java
@@ -1,8 +1,5 @@
 package ch.uzh.ifi.hase.soprafs22.rest.mapper;
 
-import ch.uzh.ifi.hase.soprafs22.entity.User;
-import ch.uzh.ifi.hase.soprafs22.rest.dto.UserGetDTO;
-import ch.uzh.ifi.hase.soprafs22.rest.dto.UserPostDTO;
 import org.mapstruct.*;
 import org.mapstruct.factory.Mappers;
 
@@ -22,13 +19,4 @@ public interface DTOMapper {
 
   DTOMapper INSTANCE = Mappers.getMapper(DTOMapper.class);
 
-  @Mapping(source = "name", target = "name")
-  @Mapping(source = "username", target = "username")
-  User convertUserPostDTOtoEntity(UserPostDTO userPostDTO);
-
-  @Mapping(source = "id", target = "id")
-  @Mapping(source = "name", target = "name")
-  @Mapping(source = "username", target = "username")
-  @Mapping(source = "status", target = "status")
-  UserGetDTO convertEntityToUserGetDTO(User user);
 }
