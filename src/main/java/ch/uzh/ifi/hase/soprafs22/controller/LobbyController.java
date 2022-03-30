025d1d5f422714a466f5994b25da1a54cf53c491
Alessio Brazerol <alessio.bra@hotmail.ch>
2022-03-30 12:56:18 +0200

    Added declaration for createLobby and getLobby, created LobbyService class

--- src/main/java/ch/uzh/ifi/hase/soprafs22/controller/LobbyController.java
+++ src/main/java/ch/uzh/ifi/hase/soprafs22/controller/LobbyController.java
@@ -1,2 +1,37 @@
-package ch.uzh.ifi.hase.soprafs22.controller;public class LobbyController {
+package ch.uzh.ifi.hase.soprafs22.controller;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
+
+/**
+ * Lobby Controller
+ * This class is responsible for handling all REST request that are related to
+ * the lobby.
+ * The controller will receive the request and delegate the execution to the
+ * LobbyService and finally return the result.
+ */
+
+@RestController
+public class LobbyController {
+
+
+    // TODO: Add request body as specified
+    // TODO: Add implementation for creating lobby
+
+    @PostMapping("/v1/game/lobby")
+    @ResponseStatus(HttpStatus.CREATED)
+    @ResponseBody
+    public void createLobby(@RequestHeader("token") String token) {
+
+    }
+
+    // TODO: Add implementation for get lobby with id
+
+    @GetMapping("/v1/game/lobby/{id}")
+    @ResponseStatus(HttpStatus.CREATED)
+    @ResponseBody
+    public void getLobby(@RequestHeader("token") String token, @PathVariable Long id) {
+
+    }
+
 }
