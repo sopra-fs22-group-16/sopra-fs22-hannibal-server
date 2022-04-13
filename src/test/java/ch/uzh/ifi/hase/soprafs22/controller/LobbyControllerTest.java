2dc0344164c4144443654cde167b72829402bf2d
luis-tm <luis.torrejonmachado@uzh.ch>
2022-04-13 12:51:12 +0200

    socket connection; test adapted - #140

--- src/test/java/ch/uzh/ifi/hase/soprafs22/controller/LobbyControllerTest.java
+++ src/test/java/ch/uzh/ifi/hase/soprafs22/controller/LobbyControllerTest.java
@@ -15,6 +15,7 @@ import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
+import org.springframework.messaging.simp.SimpMessagingTemplate;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.request.MockHttpServletRequestBuilder;
 import org.springframework.web.server.ResponseStatusException;
@@ -41,6 +42,9 @@ class LobbyControllerTest {
 
     @MockBean
     private LobbyService lobbyService;
+
+    @MockBean
+    SimpMessagingTemplate socketMessage;
 
     @Test
     void givenLobbies_whenGetLobby_thenReturnJsonArray() throws Exception {
