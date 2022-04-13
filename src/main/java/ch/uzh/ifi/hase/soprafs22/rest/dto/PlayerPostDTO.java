0ee21e40903c19b459bffa49109658856469a11b
mkorob <maria.korobeynikova@uzh.ch>
2022-04-13 16:42:12 +0200

    merging changes

---
+++ src/main/java/ch/uzh/ifi/hase/soprafs22/rest/dto/PlayerPostDTO.java
@@ -0,0 +1,14 @@
+package ch.uzh.ifi.hase.soprafs22.rest.dto;
+
+public class PlayerPostDTO {
+    private String name;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+}
