de5ecd5b86400516a62a39112f0af0b771728c7b
paolovic <paul-philipp.luley@uzh.ch>
2022-04-23 16:45:53 +0200

    removed unnecessary interface

--- src/main/java/ch/uzh/ifi/hase/soprafs22/game/player/PlayerDecorator.java
+++ src/main/java/ch/uzh/ifi/hase/soprafs22/game/player/PlayerDecorator.java
@@ -1,20 +1,20 @@
 package ch.uzh.ifi.hase.soprafs22.game.player;
 
-import ch.uzh.ifi.hase.soprafs22.game.unit.IUnit;
+import ch.uzh.ifi.hase.soprafs22.game.units.Unit;
 
 import java.util.HashMap;
 import java.util.Map;
 
 public class PlayerDecorator extends BasePlayerDecorator {
 
-    private final Map<Long, IUnit> units;
+    private final Map<Long, Unit> units;
 
     public PlayerDecorator(IPlayer player) {
         super(player);
         this.units = new HashMap<>();
     }
 
-    public IUnit getUnitById(long id){
+    public Unit getUnitById(long id){
         return units.get(id);
     }
 }
