5f137c7fa7a462000e85af892532b6b67db9c64b
mkorob <maria.korobeynikova@uzh.ch>
2022-05-24 18:51:35 +0200

    merged with master

--- src/main/java/ch/uzh/ifi/hase/soprafs22/rest/dto/get_dto/UserLoginGetDTO.java
+++ src/main/java/ch/uzh/ifi/hase/soprafs22/rest/dto/get_dto/UserLoginGetDTO.java
@@ -1,7 +1,17 @@
 package ch.uzh.ifi.hase.soprafs22.rest.dto.get_dto;
 
 public class UserLoginGetDTO {
+
+    private long id;
     private String token;
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
 
     public String getToken() {
         return token;
