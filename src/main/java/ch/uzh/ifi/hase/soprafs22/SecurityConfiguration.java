cb41850ff8c35fa2d5ad87d24e7ac9b53fa863e1
paolovic <paul-philipp.luley@uzh.ch>
2022-05-25 17:22:58 +0200

    i dont need the other stuff locally

--- src/main/java/ch/uzh/ifi/hase/soprafs22/SecurityConfiguration.java
+++ src/main/java/ch/uzh/ifi/hase/soprafs22/SecurityConfiguration.java
@@ -3,12 +3,8 @@ package ch.uzh.ifi.hase.soprafs22;
 import ch.uzh.ifi.hase.soprafs22.service.UserPropsService;
 import org.jetbrains.annotations.NotNull;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.Import;
-import org.springframework.http.HttpMethod;
 import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
@@ -17,18 +13,10 @@ import org.springframework.security.config.annotation.web.configuration.WebSecur
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
-import org.springframework.web.cors.CorsConfiguration;
-import org.springframework.web.cors.CorsConfigurationSource;
-import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
-
-import java.util.Arrays;
-import java.util.List;
 
 @Configuration
 @EnableWebSecurity
 public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
-    @Value("${api.version}")
-    private String apiVersion;
 
     @Override
     protected void configure(@NotNull HttpSecurity httpSecurity) throws Exception {
@@ -37,29 +25,6 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
                 .and().cors();
         httpSecurity.csrf().disable();
         httpSecurity.headers().frameOptions().disable();
-    }
-
-    private static final String ORIGIN_LOCALHOST = "http://localhost:3000";
-    private static final String ORIGIN_PROD = "https://sopra-fs22-group-16-client.herokuapp.com";
-
-
-
-    @Bean
-    CorsConfigurationSource corsConfigurationSource() {
-        final UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
-        final CorsConfiguration configuration = new CorsConfiguration();
-        configuration.setAllowCredentials(true);
-        configuration.setExposedHeaders(Arrays.asList("Authorization"));
-        configuration.setAllowedOrigins(List.of(ORIGIN_LOCALHOST, ORIGIN_PROD));
-        configuration.addAllowedMethod("*");
-        configuration.addAllowedHeader("*");
-        configuration.applyPermitDefaultValues();
-        source.registerCorsConfiguration("/"+apiVersion+"/**", configuration);
-        source.registerCorsConfiguration("/"+apiVersion+"/*", configuration);
-        source.registerCorsConfiguration("/v1/*", configuration);
-        source.registerCorsConfiguration("/v1/*", configuration);
-        source.registerCorsConfiguration("/hannibal-websocket/**", configuration);
-        return source;
     }
 
     @Bean
--- src/main/java/ch/uzh/ifi/hase/soprafs22/socket/Socket.java
+++ src/main/java/ch/uzh/ifi/hase/soprafs22/socket/Socket.java
@@ -3,7 +3,6 @@ package ch.uzh.ifi.hase.soprafs22.socket;
 import org.jetbrains.annotations.NotNull;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.messaging.simp.config.MessageBrokerRegistry;
-import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker;
 import org.springframework.web.socket.config.annotation.StompEndpointRegistry;
 import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerConfigurer;
