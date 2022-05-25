79eaab418c21cbc93c68cff3eea26693552504f2
paolovic <paul-philipp.luley@uzh.ch>
2022-05-25 12:41:19 +0200

    removed logout endpoint

--- src/main/java/ch/uzh/ifi/hase/soprafs22/controller/UserController.java
+++ src/main/java/ch/uzh/ifi/hase/soprafs22/controller/UserController.java
@@ -61,17 +61,6 @@ public class UserController {
         return DTOMapper.INSTANCE.convertRegisteredUserToUserLoginGetDTO(registeredUser);
     }
 
-    @PostMapping("/{apiVersion}/logout")
-    @ResponseStatus(HttpStatus.OK)
-    @ResponseBody
-    public UserLoginGetDTO logoutUser(@RequestBody RegisteredUserPutDTO registeredUserPutDTO){
-        RegisteredUser userInput = DTOMapper.INSTANCE.convertRegisteredUserPutDTOToRegisteredUser(registeredUserPutDTO);
-
-        RegisteredUser registeredUser = userService.loginUser(userInput);
-
-        return DTOMapper.INSTANCE.convertRegisteredUserToUserLoginGetDTO(registeredUser);
-    }
-
     @GetMapping("/{apiVersion}/users")
     @ResponseStatus(HttpStatus.OK)
     @Validated
